56706037b4a7c9ab3b05ead92a42a4ed2f0a7ed3
Don Kelly <karfai@gmail.com>
2017-12-06 16:17:22 +0400

    Setup Travis configuration
    Change-Id: I1ed2b87b505295f7e4e344799c60140b5b14ed92

--- build_images.sh
+++ build_images.sh
@@ -5,7 +5,7 @@ fi
 
 VERSION=`cat .version`
 
-docker build -t "xalgorithms/xadf-produce-service:latest-$ENVIRONMENT" -t "xalgorithms/xadf-produce-service:$VERSION-$ENVIRONMENT" -f "Dockerfile.$ENVIRONMENT" .
+docker build -t "xalgorithms/xadf-revisions-service:latest-$ENVIRONMENT" -t "xalgorithms/xadf-revisions-service:$VERSION-$ENVIRONMENT" -f "Dockerfile.$ENVIRONMENT" .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push "xalgorithms/xadf-produce-service:latest-$ENVIRONMENT"
-docker push "xalgorithms/xadf-produce-service:$VERSION-$ENVIRONMENT"
+docker push "xalgorithms/xadf-revisions-service:latest-$ENVIRONMENT"
+docker push "xalgorithms/xadf-revisions-service:$VERSION-$ENVIRONMENT"
