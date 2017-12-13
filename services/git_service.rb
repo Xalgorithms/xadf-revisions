f09f0fa70b399b18058b65c58ff2d8ff45134b64
Hayk Pilosyan <hayk.pilos@gmail.com>
2017-12-13 18:01:19 +0400

    Validate the package
    Change-Id: I516f25441cc38e92c553bd5c3f039a7d81d11c16

--- services/git_service.rb
+++ services/git_service.rb
@@ -5,13 +5,13 @@ include XA::Rules::Parse
 
 class GitService
   def self.init(clone_url, repo_name)
-    repo = Rugged::Repository.clone_at(clone_url, File.join('.', repo_name), bare: true)
-    # repo = Rugged::Repository.discover("./libgit2-test")
-    success = parse_all(repo, scan_all(repo))
+    # repo = Rugged::Repository.clone_at(clone_url, File.join('.', repo_name), bare: true)
+    repo = Rugged::Repository.discover("./libgit2-test")
+    res = parse_all(repo, scan_all(repo))
 
-    clean(repo_name)
+    # clean(repo_name)
 
-    success
+    res
   end
 
   def self.clean(repo_name)
@@ -20,38 +20,80 @@ class GitService
 
   private
 
+  def self.is_all_valid_extension?(files)
+    files.all? {|o| is_valid_extension?(o[:name])}
+  end
+
+  def self.is_valid_extension?(name)
+    ext = File.extname(name)
+    Sinatra::Application.settings.allowed_formats.include?(ext[1..-1])
+  end
+
   def self.is_rule_file?(name)
     ext = File.extname(name)
-    ext[1..-1].to_sym == :xalgo
+    ext[1..-1].to_sym == :rule
+  end
+
+  def self.is_package_file?(name)
+    ext = File.extname(name)
+    ext[1..-1].to_sym == :package
   end
 
   def self.scan_all(repo)
-    br = repo.branches['master']
-    all_files = []
+    br = repo.branches[Sinatra::Application.settings.git_branch]
+    packages = {}
     
+    # Iterate through the packages
     br.target.tree.each_tree do |o|
       files = repo.lookup(o[:oid]).inject([]) do |fs, o|
-        name = o[:name]
-        is_rule_file?(name) ? fs.push(o[:oid]) : fs
+        fs.push o
       end
-      all_files.concat files
+
+      if is_all_valid_extension?(files)
+        id = o[:oid]
+        packages[id] = files
+      end
     end
 
-    all_files
+    packages
   end
 
   def self.parse_all(repo, all)
-    all_lines = []
-    all.each do |oid|
+    res = all.map do |id, files|
+      if invalid_package_files repo, files
+        return false
+      end
+
+      rule_file = files.find { |f| is_rule_file?(f[:name]) }
+      oid = rule_file[:oid]
       blob = repo.lookup(oid)
       lines = blob.content.lines.map do |line|
         parse_single line.strip
       end
 
-      all_lines.concat lines
+      lines.all? ? lines : false
     end
 
-    all_lines.all?
+    res
+  end
+
+  def self.get_rule_file_name(name)
+    File.basename name, ".*"
+  end
+
+  def self.invalid_package_files(repo, files)
+    meta_file = files.find { |f| is_package_file?(f[:name]) }
+    meta_oid = meta_file[:oid]
+
+    blob = repo.lookup(meta_oid)
+
+    meta = JSON.parse(blob.content)
+    rules = meta["rules"]
+    rules_files = files
+      .select { |f| is_rule_file? f[:name] }
+      .select { |f| rules.keys.include?(get_rule_file_name(f[:name]))}
+
+    rules_files.length == 0
   end
 
   def self.parse_single(line)
