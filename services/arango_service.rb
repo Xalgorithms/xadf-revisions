f0fd5b2cbcaa48e4048388177f160c63576a1853
Hayk Pilosyan <hayk.pilos@gmail.com>
2017-12-18 01:07:48 +0400

    Check rule version
    Change-Id: I2b7a3fdf13235cb4d93821dca13644624e0aab86

--- services/arango_service.rb
+++ services/arango_service.rb
@@ -15,11 +15,35 @@ class ArangoService
     @database = ArangoDatabase.new.retrieve
   end
 
-  def self.store_rule(key, rule)
+  def self.store_new_rule_version(key, rule_id, version, rule)
+    if is_new_rule_version?(rule_id, version)
+      store_rule key, rule_id, version, rule
+    end
+  end
+
+  def self.store_rule(key, rule_id, version, rule)
     collection = @database["rules"]
-    doc = {_key: key}.merge({items: rule})
+    doc = {_key: key, rule_id: rule_id, version: version}.merge({items: rule})
     res = collection.create_document(document: doc)
 
     res.key
+  end
+
+  def self.get_rule_by_id(id)
+    collection = @database["rules"]
+
+    collection.documentMatch match: {"rule_id" => id}
+  end
+
+  def self.is_new_rule_version?(rule_id, version)
+    rule = get_rule_by_id rule_id
+    if rule == "no match"
+      return true
+    end
+
+    old_version_1, old_version_2, old_version_3 = rule.body["version"].split "."
+    new_version_1, new_version_2, new_version_3 = version.split "."
+
+    return new_version_1 >= old_version_1 && new_version_2 >= old_version_2 && new_version_3 > old_version_3
   end
 end
