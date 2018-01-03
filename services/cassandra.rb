f0bd13b531127de30d2a2342be13f44323cc8112
Don Kelly <karfai@gmail.com>
2018-01-03 21:21:36 +0400

    Commits to Cassandra using BATCH insertion
    Change-Id: I55a875814ecb0e527784b28eaac929c6006f7969

--- services/cassandra.rb
+++ services/cassandra.rb
@@ -10,23 +10,34 @@ module Services
       @session = cluster.connect(opts['keyspace'])
     end
 
-    def store_effective(o)
-      store_json('xadf.effective', o)
+    def store_effectives(os)
+      keys = [:country, :region, :timezone, :starts, :ends, :party, :rule_id]
+      within_batch do
+        build_inserts('xadf.effective', keys, os)
+      end
     end
 
-    def store_when_key(o)
-      store_json('xadf.when_keys', o)
-    end
-
-    def store_when(o)
-      store_json('xadf.whens', o)
+    def store_whens(os)
+      within_batch do
+        build_inserts('xadf.when_keys', [:section, :key], os) + 
+          build_inserts('xadf.whens',  [:section, :key, :op, :val, :rule_id], os)
+      end
     end
 
     private
 
-    def store_json(tn, o)
-      stm = @session.prepare("INSERT INTO #{tn} JSON ?")
-      @session.execute(stm, arguments: [MultiJson.encode(o)])
+    def build_inserts(tn, ks, os)
+      os.map do |o|
+        avail_ks = ks.select { |k| o.key?(k) && o[k] }
+        vals = avail_ks.map { |k| "'#{o[k]}'" }
+        "INSERT INTO #{tn} (#{avail_ks.join(',')}) VALUES (#{vals.join(',')})"
+      end.join('; ') + ';'
+    end
+    
+    def within_batch
+      q = 'BEGIN BATCH ' + yield + ' APPLY BATCH;'
+      stm = @session.prepare(q)
+      @session.execute(stm)
     end
   end
 end
--- services/translate.rb
+++ services/translate.rb
@@ -12,36 +12,40 @@ module Services
     def translate_effective(id)
       @documents.find_meta(id) do |doc|
         if 'rule' == doc['type']
-          o = {
-            rule_id: id,
-            country: doc.fetch('jurisdiction', {}).fetch('country', nil),
-            region: doc.fetch('jurisdiction', {}).fetch('region', nil),
-            party: doc.fetch('party', 'any'),
-          }
-          doc.fetch('effective', []).each do |eff|
-            @cassandra.store_effective(
-              o.merge(timezone: eff['timezone'], starts: eff['starts'], ends: eff['ends']))
+          effs = doc.fetch('effective', []).map do |eff|
+            {
+              rule_id:  id,
+              country:  doc.fetch('jurisdiction', {}).fetch('country', nil),
+              region:   doc.fetch('jurisdiction', {}).fetch('region', nil),
+              party:    doc.fetch('party', 'any'),
+              timezone: eff['timezone'],
+              starts:   eff['starts'],
+              ends:     eff['ends'],
+            }
           end
+          @cassandra.store_effectives(effs)
         end
       end
     end
 
     def translate_whens(id)
       @documents.find_rule(id) do |doc|
-        doc['whens'].fetch('envelope', []).each do |wh|
+        whens = doc['whens'].fetch('envelope', []).map do |wh|
           expr = wh['expr']
           # NOTE: for now, the parser guarantees us that the left is
           # the reference and the right is the value to
           # match... Assuming this is very fragile thinking that
           # should eventually be changed
-          @cassandra.store_when_key(section: expr['left']['section'], key: expr['left']['key'])
-          @cassandra.store_when(
+          {
             section: expr['left']['section'],
-            key: expr['left']['key'],
-            op: expr['op'],
-            val: expr['right']['value'],
-            rule_id: id)
+            key:     expr['left']['key'],
+            op:      expr['op'],
+            val:     expr['right']['value'],
+            rule_id: id
+          }
         end
+
+        @cassandra.store_whens(whens)
       end
     end
   end
